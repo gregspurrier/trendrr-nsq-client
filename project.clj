0babc0f4eef251a9eb1c9503d94422c579be8160
Greg Spurrier <greg@sourcematters.org>
2015-03-04 09:23:25 -0800

    Initial commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject org.clojars.gregspurrier/trendrr-nsq-client "1.2.0-SNAPSHOT"
+  :description "Prebuilt JARs for TrendrrNSQClient and its dependency trendrr-oss"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT"}
+  :dependencies [[ch.qos.logback/logback-classic "1.0.13"]
+                 [ch.qos.logback/logback-core "1.0.13"]
+                 [com.google.guava/guava "13.0.1"]
+                 [commons-logging/commons-logging "1.1.3"]
+                 [io.netty/netty "3.6.1.Final"]
+                 [org.slf4j/slf4j-api "1.6.4"]]
+  :java-source-paths ["vendor/github/mreiferson/TrendrrNSQClient/src/main/java"
+                      "vendor/github/trendrr/java-oss-lib/src/main/java"])
