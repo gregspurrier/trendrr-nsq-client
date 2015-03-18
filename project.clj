c4581d0d83721791e814bba007f74ff316bdc04a
Greg Spurrier <greg@sourcematters.org>
2015-03-18 14:24:57 -0700

    Update Netty dependency to match TrendrrNSQClient

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [ch.qos.logback/logback-core "1.0.13"]
                  [com.google.guava/guava "13.0.1"]
                  [commons-logging/commons-logging "1.1.3"]
-                 [io.netty/netty "3.6.1.Final"]
+                 [io.netty/netty "3.6.10.Final"]
                  [org.slf4j/slf4j-api "1.6.4"]]
   :java-source-paths ["vendor/github/gregspurrier/TrendrrNSQClient/src/main/java"
                       "vendor/github/trendrr/java-oss-lib/src/main/java"])
